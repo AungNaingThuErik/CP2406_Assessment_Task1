3d2e51c93abb92e2ab5e3379c74d43e6e5e07b51
Aung Naing Thu <aungnaingthu2727@gmail.com>
2020-01-24 20:47:21 +0800

    BusClass

---
+++ src/Bus.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+
+public class Bus extends Vehicle {
+    Bus(Road road){
+        super(road);
+        width = 45;
+        height = 15;
+    }
+    public void paintMeHorizontal(Graphics g){
+        g.setColor(Color.GREEN);
+        g.fillRect(xPos, yPos, width, height);
+    }
+    public void paintMeVertical(Graphics g){
+        g.setColor(Color.GREEN);
+        g.fillRect(yPos, xPos, height, width);
+    }
+}
